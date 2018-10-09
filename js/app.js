51ceaf544751c7ba2e0c1eb16b145f7e7d0b8246
crownbackend <crownbackend@gmail.com>
2018-10-09 16:39:11 +0200

    ajax

--- js/app.js
+++ js/app.js
@@ -2,40 +2,32 @@ $(document).ready(function(){
 
     $("#target").append('lorem <br>');
 
-    let texte = "recoucou";
-    $("#target").append(texte + " ça va toi ?");
+    let tasks;
+    tasks = JSON.parse(localStorage.getItem("tasks"));
 
-    function teste(args)
-    {
-        console.log('salut à tous ' + args);
-    }
-
-    teste('je suis moi');
-
-
-    let tasks = [];
     function showTasks()
     {
         $("#result").empty();
         for(let i = 0; i < tasks.length; i++)
         {
-            $("#result").append("<li class='select list-group-item' data-target='+ i +'>" + tasks[i] + "<button class='delete btn btn-danger'> x</button>" +
+            $("#result").append("<li class='select list-group-item' data-target='+ i +'>"
+                    + tasks[i] +
+                    "<button class='delete btn btn-danger'> x</button>" +
                 "</li>");
         }
-
+        localStorage.setItem('tasks', JSON.stringify(tasks));
     }
     $("#btnClick").click(function(){
 
         let value = $("#filed").val();
         if(value.length > 0)
         {
-
-            tasks.push(value);
             $("#error").slideUp('slow');
             $('#filed').val('');
             $("#result").append("<li class='select list-group-item'>" + value +
             "<button class='delete btn btn-danger'> x</button>" +
             "</li>");
+            tasks.push(value);
             showTasks();
         }
         else
@@ -62,6 +54,25 @@ $(document).ready(function(){
         $(this).removeClass('result2');
     });*/
 
+    $("button").click(function(){
+    $.ajax({
+        type: 'GET',
+        url: 'https://api.punkapi.com/v2/beers/random',
+        timeout: 3000,
+        success: function(data){
+            $('#zone').append(data);
+            for(i = 0; i < data.length; i++)
+            {
+                let beer = data[i];
+                $("#zone").append(beer.name + beer.id);
+            }
+        },
+        error: function(){
+            $('#zone').html('Cette requête AJAX n\'a pas abouti');
+        }
+    });
+
+    });
 
 
 
